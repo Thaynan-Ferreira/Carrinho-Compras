d106b834fb2d3cb4ceae949c54ad3bb822f8d535
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-24 10:08:00 -0300

    Implementando o limpar

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
+let produtoSelecionado = document.querySelector('#produto');
+let quantidade = document.querySelector('#quantidade');
+let listaProduto = document.querySelector('#lista-produtos');
+let total = document.querySelector('.carrinho__total');
+
 let soma = 1400;
 function adicionar(){
-    let produtoSelecionado = document.querySelector('#produto');
-    let quantidade = document.querySelector('#quantidade');
-    let listaProduto = document.querySelector('#lista-produtos');
     let novaSection = document.createElement('section');
     novaSection.className = 'carrinho__produtos__produto';
-    let total = document.querySelector('.carrinho__total');
-
 
     let valorProduto = 0;
     let totalProduto = 0
@@ -31,7 +31,14 @@ function adicionar(){
     console.log(totalProduto);
     novaSection.innerHTML = `<span class="texto-azul">${quantidade.value}x</span> ${nomeProduto} <span class="texto-azul">R$${totalProduto}</span>`;
     listaProduto.appendChild(novaSection);
+
     soma += totalProduto
     total.innerHTML = `Total: <span class="texto-azul" id="valor-total">R$${soma}</span>`
 }
 
+function limpar() {
+    listaProduto.innerHTML = ''
+    soma = 0;
+
+    total.innerHTML = `Total: <span class="texto-azul" id="valor-total">R$${soma}</span>`
+}
